0b5fcdbf07e8e2fa7b26f8ab6b3941c47b51544e
Joshua T. Fox <joshua@doit-intl.com>
2022-08-26 16:10:01 +0300

    DRY in test

--- test/ferent/find_cycles_test.clj
+++ test/ferent/find_cycles_test.clj
@@ -5,13 +5,19 @@
 (deftest test-find-cycles
   (testing "Find Cycles"
     (are [expect adj-map]
-         (= expect (find-cycles adj-map))
+      (= expect (find-cycles adj-map))
       [["a" "b"]]
       {"a" ["b"], "b" ["a"] "c" ["a"]}
       [["a" "b"]]
       {"a" ["b"], "b" ["a"]}
       [["a" "b" "c"] ["a" "b" "c" "d"]]
       {"a" #{"b"} "b" #{"c" "d"} "c" #{"a"} "d" #{"c"}}
+      [["a"]]
+      {"a" ["a"], "b" ["c"]}
+      []
+      {"a" ["b"], "b" ["c"]}
+      [["b" "c"]]
+      {"a" ["b"], "b" ["c"] "c" ["b"]}
       [["x"] ["a" "b"] ["c" "d" "e" "f"] ["p" "q" "r" "s"]]
       {"x" ["x"] "a" ["b"] "b" ["a"] "c" ["d"] "d" ["e"] "e" ["f"]
        "f" ["c"] "p" ["q"] "q" ["r"] "r" ["s"] "s" ["p"]})))
