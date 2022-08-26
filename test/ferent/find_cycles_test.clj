5427b54f852a9aa7ef16635cb4267673633b8b05
Joshua T. Fox <joshua@doit-intl.com>
2022-08-26 16:06:58 +0300

    DRY in test

--- test/ferent/find_cycles_test.clj
+++ test/ferent/find_cycles_test.clj
@@ -2,46 +2,16 @@
   (:require [clojure.test :refer :all]
             [ferent.cycles :refer [find-cycles]]))
 
-(def adj-map1 {"x" ["x"]
-               "a" ["b"]
-               "b" ["a"]
-               "c" ["d"]
-               "d" ["e"]
-               "e" ["f"]
-               "f" ["c"]
-               "p" ["q"]
-               "q" ["r"]
-               "r" ["s"]
-               "s" ["p"]})
-
-(def adj-map2 {"a" ["b"],
-               "b" ["a"]
-               "c" ["a"]})
-
-(def adj-map3 {"a" ["b"],
-               "b" ["a"]})
-
-(def adj-map4 {"a" #{"b"}
-               "b" #{"c" "d"}
-               "c" #{"a"}
-               "d" #{"c"}})
-
 (deftest test-find-cycles
   (testing "Find Cycles"
-    (is (=
-         [["x"] ["a" "b"] ["c" "d" "e" "f"] ["p" "q" "r" "s"]]
-         (find-cycles adj-map1)))
-    (is (=
-         [["a" "b"]]
-         (find-cycles adj-map2)))
-    (is (=
-         [["a" "b"]]
-         (find-cycles adj-map3)))
-    (is (=
-         [["a" "b" "c"] ["a" "b" "c" "d"]]
-         (find-cycles adj-map4)))))
-
-
-
-
-
+    (are [expect adj-map]
+         (= expect (find-cycles adj-map))
+      [["a" "b"]]
+      {"a" ["b"], "b" ["a"] "c" ["a"]}
+      [["a" "b"]]
+      {"a" ["b"], "b" ["a"]}
+      [["a" "b" "c"] ["a" "b" "c" "d"]]
+      {"a" #{"b"} "b" #{"c" "d"} "c" #{"a"} "d" #{"c"}}
+      [["x"] ["a" "b"] ["c" "d" "e" "f"] ["p" "q" "r" "s"]]
+      {"x" ["x"] "a" ["b"] "b" ["a"] "c" ["d"] "d" ["e"] "e" ["f"]
+       "f" ["c"] "p" ["q"] "q" ["r"] "r" ["s"] "s" ["p"]})))
